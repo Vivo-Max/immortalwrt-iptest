9df1299ccb96b1a404a0ed7f9a27ac91d7d00d19
M_107 <angxyaa@gmail.com>
2025-12-20 22:09:41 +0800

    Delete luci-app-iptest directory

--- luci-app-iptest/files/luasrc/controller/iptest.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.iptest", package.seeall)
-
-local i18n = require "luci.i18n"
-local t = i18n.translate
-
-function index()
-    entry({"admin", "services", "iptest"}, cbi("iptest"), t("Cloudflare IP测试"), 100).dependent = true
-    entry({"admin", "services", "iptest", "log"}, view("iptest/log"), t("运行日志"), 2)
-end
